a55a1a185e298ea94a82ce8f9d3348f6c88fcb52
B1loliddin <bilolbotirov000@gmail.com>
2023-09-27 14:17:46 +0500

    Code refactored

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:yandex_mapkit_in_flutter/app.dart';
 import 'package:yandex_mapkit_in_flutter/core/service_locator.dart';
-import 'package:yandex_mapkit_in_flutter/presentation/pages/get_current_location_page.dart';
-import 'package:yandex_mapkit_in_flutter/presentation/pages/polygon_page.dart';
-import 'package:yandex_mapkit_in_flutter/presentation/pages/polyline_page.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,19 +8,4 @@ void main() {
   serviceLocator();
 
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.light(useMaterial3: true),
-      darkTheme: ThemeData.dark(useMaterial3: true),
-      themeMode: ThemeMode.dark,
-      home: const GetCurrentLocationPage(),
-    );
-  }
 }
